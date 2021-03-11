7a4f5fca5900159feabd4a23ac7f1b160daf24b5
annahlv <annahlv.work@gmail.com>
2021-03-11 14:13:15 +0200

    feat: add some code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = [];
+  let brackets = bracketsConfig.reduce((acc, val) => acc.concat(val), []);
+  let opening = brackets.filter((_, i) => i % 2 == 0);
+  let closing = brackets.filter((_, i) => i % 2 != 0);
+  for (let i = 0; i < str.length; i++) {
+    if (opening.includes(str[i])) {
+      array.push(str[i]); 
+    } else if (closing.includes(str[i])) {
+      if (array.pop() !== str[i-1]) return false;
+    }
+  }
+  //if (array.length !== 0) return false;
+  return true;
 }
