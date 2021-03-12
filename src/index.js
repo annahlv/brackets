7620263b0d45fc753bf9924e1e24af5aa1de3a46
annahlv <annahlv.work@gmail.com>
2021-03-12 15:52:50 +0200

    fix: adjust test results

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
+  console.log(str);
   let array = [];
   let brackets = bracketsConfig.reduce((acc, val) => acc.concat(val), []);
   let opening = brackets.filter((_, i) => i % 2 == 0);
   let closing = brackets.filter((_, i) => i % 2 != 0);
+  console.log(str);
   for (let i = 0; i < str.length; i++) {
     if (opening.includes(str[i])) {
       array.push(str[i]); 
-    } else if (closing.includes(str[i])) {
-      if (array.pop() !== str[i-1]) return false;
+    } 
+    if (closing.includes(str[i])) {
+      let last = array.pop();
+      //console.log(str[i-1], last);
+      if (str[i - 1] !== last) array.pop();
     }
   }
-  //if (array.length !== 0) return false;
+  if (array.length !== 0) return false;
   return true;
 }
